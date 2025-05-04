b580d22cd112ecc4197b535ec17844ae6dc97366
Prashant20580917 <www.prashantp2058@gmail.com>
2025-05-04 18:45:25 +0545

    Initial commit - LMS project with client and server

---
+++ client/src/pages/educator/Educator.jsx
@@ -0,0 +1,24 @@
+import Footer from '@/components/educator/Footer'
+import Navbar from '@/components/educator/Navbar'
+import Sidebar from '@/components/educator/Sidebar'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Educator = () => {
+  return (
+    <div className='text-default min-h-screen bg-white'>
+        <Navbar/>
+        <div className='flex'>
+          <Sidebar/>
+
+          <div className='flex-1'>
+          {<Outlet/>}
+          </div>
+
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Educator
---
+++ server/server.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
+import connectCloudinary from './configs/cloudinary.js'
+import courseRouter from './routes/courseRoutes.js'
+import userRouter from './routes/userRoutes.js'
+
+
+//Initialize Express
+const app = express()
+
+// Connect to Database 
+await connectDB()
+await connectCloudinary()
+
+//Middleware
+// app.use(cors())
+app.use(clerkMiddleware())
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true
+  }))
+
+
+//Routes
+app.get('/', (req,res)=> res.send("API Working"))
+app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(),educatorRouter)
+app.use('/api/course',express.json(),courseRouter)
+app.use('/api/user',express.json(),userRouter)
+app.post('/stripe',express.raw({type:'application/json'}), stripeWebhooks)
+
+//Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on Port ${PORT}`)
+})
